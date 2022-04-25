a9f89bf7e29f6074129f98dda0cbf698333f3838
Naveen Yaragarla <naveen.yaragarla@gmail.com>
2022-04-25 14:45:25 -0400

    feat: adding the react native elements component library

--- app/features/carListings/ListingsScreen.tsx
+++ app/features/carListings/ListingsScreen.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import { CarStackParamList, MainBottomTabParamList } from '../../utils/types';
+import { View, Text, StyleSheet } from 'react-native';
+import { Button } from '@rneui/themed';
+import { CarStackParamList } from '../../utils/types';
 import { useNavigation } from '@react-navigation/native';
-import { StackNavigationProp } from "@react-navigation/stack";
+import { StackNavigationProp } from '@react-navigation/stack';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,6 +21,16 @@ const ListingsScreen: React.FC<CarStackParamList> = () => {
       <Text>Car Listings Screen</Text>
       <Button
         title='Details'
+        buttonStyle={{
+          borderColor: 'rgba(78, 116, 289, 1)',
+        }}
+        type='outline'
+        titleStyle={{ color: 'rgba(78, 116, 289, 1)' }}
+        containerStyle={{
+          width: 200,
+          marginHorizontal: 50,
+          marginVertical: 10,
+        }}
         onPress={() => navigation.navigate('Details')}
       />
     </View>
